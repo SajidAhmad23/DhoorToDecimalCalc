fc3594fe1eaa94e4b5db94bbc496f8865518445d
Saji-Rohit <saji.ahmad20221@gmail.com>
2023-06-26 08:35:53 +0530

    decimal to dhur

---
+++ script.js
@@ -0,0 +1,27 @@
+function convertToDhoor() {
+  var laggi = parseFloat(document.getElementById("laggi-input-d").value);
+
+  if (isNaN(laggi)) {
+    document.getElementById("result-d").innerHTML = "Please enter a valid number for Laggi.";
+    return;
+  }
+
+  var decimal = 1;
+  var dhoor = (decimal * 968) / (5 * Math.pow(laggi, 2));
+
+  document.getElementById("result-d").innerHTML = "Dhoor: " + dhoor.toFixed(2);
+}
+
+function convertToDecimal() {
+  var dhoor = parseFloat(document.getElementById("dhoor-input").value);
+  var laggi = parseFloat(document.getElementById("laggi-input-d2").value);
+
+  if (isNaN(dhoor) || isNaN(laggi)) {
+    document.getElementById("result-d2").innerHTML = "Please enter valid numbers.";
+    return;
+  }
+
+  var decimal = (dhoor * 5 * Math.pow(laggi, 2)) / 968;
+
+  document.getElementById("result-d2").innerHTML = "Decimal: " + decimal.toFixed(2);
+}
