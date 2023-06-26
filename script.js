826695d3a8bb547201ef38035f2f4c8e29ebb201
Saji-Rohit <saji.ahmad20221@gmail.com>
2023-06-26 08:56:09 +0530

    decimal to dhoor

--- script.js
+++ script.js
@@ -25,3 +25,19 @@ function convertToDecimal() {
 
   document.getElementById("result-d2").innerHTML = "Decimal: " + decimal.toFixed(2);
 }
+
+function convertDecimalAndDhoor() {
+  var laggi = parseFloat(document.getElementById("laggi-input-d3").value);
+  var decimal = parseFloat(document.getElementById("decimal-input-d3").value);
+
+  if (isNaN(laggi) || isNaN(decimal)) {
+    document.getElementById("result-d3").innerHTML = "Please enter valid numbers.";
+    return;
+  }
+
+  var dhoor = (decimal * 968) / (5 * Math.pow(laggi, 2));
+
+  document.getElementById("result-d3").innerHTML = "Dhoor: " + dhoor.toFixed(2);
+}
+
+document.getElementById("year").textContent = new Date().getFullYear();
